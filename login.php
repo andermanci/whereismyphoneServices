f4c537caec01fe8085b9be90b5dafc8f9e2702a7
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 21:54:13 +0200

    Lehen igoera, 2 proba

--- login.php
+++ login.php
@@ -19,7 +19,10 @@
 			exit;
 		 }
 		$email=($_GET['email']);
+		echo $email;
+
 		$pass=sha1.($_GET['pass']);
+		echo $pass;
 		$query = $pdo->prepare("Select * from users where email= '" . $email . "' and password= '" . $pass . "' ;" );
 		if($query->execute()){
 			    if ($row = $query->fetch(PDO::FETCH_ASSOC)){
