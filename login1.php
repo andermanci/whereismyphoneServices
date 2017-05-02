5babf05821617f31ed82e2e72adb99720822ba41
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 23:01:04 +0200

    Lehen igoera, 2 proba

--- login1.php
+++ login1.php
@@ -6,24 +6,5 @@
 		}
 
 
-		$email=($_GET['email']);
-		echo $email;
-
-		$pass=sha1($_GET['pass']);
-		echo $pass;
-
-        echo "a";
-        $DataBase ="host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;dbname=d32n963hbr8ngb;user=ofwsfkgedzztju;port=5432;sslmode=require;password=fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98;";
-        $db = new PDO($DataBase);
-        $query="Select * from users where email= '" . $email . "' and password= '" . $pass . "' ;" ;
-        $result = $db->query($query);
-        $row = $result->fetch(PDO::FETCH_ASSOC)
-         if($row){
-            echo $row["id"];
-        }
-        else{
-             echo "error1";
-        }
-        $result->closeCursor();
  ?>
 
