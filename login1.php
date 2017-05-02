494ed299e8429b456c978297b52e46465df46119
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 23:05:35 +0200

    Lehen igoera, 2 proba

--- login1.php
+++ login1.php
@@ -13,6 +13,8 @@
 		echo $pass;
 
         echo "a";
-      
+        $DataBase ="host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;dbname=d32n963hbr8ngb;user=ofwsfkgedzztju;port=5432;sslmode=require;password=fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98;";
+        echo $DataBase;
+        
  ?>
 
