506f8f3b8e8c6358abb434c22423798a9a0c63ae
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 22:28:06 +0200

    Lehen igoera, 2 proba

--- login.php
+++ login.php
@@ -5,13 +5,12 @@
 			exit;
 		}
 		try{
-				$dbopts = parse_url(getenv('postgres://ofwsfkgedzztju:fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98@ec2-79-125-125-97.eu-west-1.compute.amazonaws.com:5432/d32n963hbr8ngb'));
 				echo "a";
 				$app->register(new Herrera\Pdo\PdoServiceProvider(),
 							   array(
-								   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-								   'pdo.username' => $dbopts["user"],
-								   'pdo.password' => $dbopts["pass"]
+								   'pdo.dsn' => 'pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432',
+								   'pdo.username' => "ofwsfkgedzztju",
+								   'pdo.password' =>"fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98"
 							   )
 				);
 				echo "b";
