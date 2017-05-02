0216a70654ba5a33b0d65fff8fa009ca2b927310
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 22:00:14 +0200

    Lehen igoera, 2 proba

--- login1.php
+++ login1.php
@@ -5,7 +5,7 @@
 			exit;
 		}
 		try{
-				$dbopts = parse_url(getenv('postgres://ofwsfkgedzztju:fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98@ec2-79-125-125-97.eu-west-1.compute.amazonaws.com:5432/d32n963hbr8ngb'));
+				$dbopts = parse_url(getenv('DATABASE_URL'));
 				echo "a";
 				$app->register(new Herrera\Pdo\PdoServiceProvider(),
 							   array(
