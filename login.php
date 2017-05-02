20a17bd54552a9cdb45f8592cb31bb2d87293c64
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 21:18:02 +0200

    Lehen igoera

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+
+		if( !(isset($_GET['email']) and isset($_GET['pass']))){
+			echo "error";
+			exit;
+		}
+		try{
+				$dbopts = parse_url(getenv('DATABASE_URL'));
+				$app->register(new Herrera\Pdo\PdoServiceProvider(),
+							   array(
+								   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+								   'pdo.username' => $dbopts["user"],
+								   'pdo.password' => $dbopts["pass"]
+							   )
+				);
+		}   
+		catch (PDOException $e) {
+			echo "error";
+			exit;
+		 }
+		$email=($_GET['email']);
+		$pass=sha1.($_GET['pass']);
+		$query = $pdo->prepare("Select * from users where email= '" . $email . "' and password= '" . $pass . "' ;" );
+		if($query->execute()){
+			    if ($row = $query->fetch(PDO::FETCH_ASSOC)){
+					echo $row['id'] ;       
+				}
+				else{
+					echo "error";
+				}			
+		}
+		else{
+			echo "error";
+		}
+		
+?>
