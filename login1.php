3a6dfbe4c2d8b291c5f5d7ffc95ce459901a4687
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-02 22:15:06 +0200

    Lehen igoera, 2 proba

--- login1.php
+++ login1.php
@@ -6,25 +6,14 @@
 		}
 
 
-		
+
 		$email=($_GET['email']);
 		echo $email;
 
-		$pass=sha1.($_GET['pass']);
+		$pass=sha1($_GET['pass']);
 		echo $pass;
-		$query="Select * from users where email= '" . $email . "' and password= '" . $pass . "' ;" ;
-        $result=pg_query($DataBase, $query);
-		if(!$result){
-			$select =pg_fetch_array( $result );
-			echo $select[0];
-		}
-		else{
-			echo "error2";
-		}
-		pg_close($DataBase);
-		}
-		else{
-			echo "error3";
-		}
+
+
+        
 		
 ?>
