841b10ee2b35b49584e56d05587185b6ee09c626
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-03 21:58:58 +0200

    Bigarren igoera, get Devices

---
+++ web/getDevices.php
@@ -0,0 +1,23 @@
+<?php
+
+		if( !(isset($_GET['id']))){
+			echo "error";
+			exit;
+		}
+
+
+		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
+
+
+		$id=($_GET['id']);
+       echo "a";
+
+
+
+
+         $sql = "Select * from device where users_id= '" . $id . "' ";
+         echo "b";
+         $devices=$dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+         echo "c";
+         echo json_encode($devices);
+?>
