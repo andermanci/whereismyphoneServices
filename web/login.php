81cc073372412ddc9e5067e4d042cc961a5ddb08
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-03 09:46:39 +0200

    Lenengo igoera

--- web/login.php
+++ web/login.php
@@ -8,7 +8,7 @@
 				echo "a";
 
 
-		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
+		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
 
 
 		$email=($_GET['email']);
