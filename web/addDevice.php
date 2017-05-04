f1a911154766e080e1b122024172feedae4dd728
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-04 22:31:14 +0200

    Hirugarren igoera, addDevice

---
+++ web/addDevice.php
@@ -0,0 +1,42 @@
+<?php
+
+		if( !(isset($_GET['id']) and !(isset($_GET['name']) and !(isset($_GET['info']) and !(isset($_GET['token']) )){
+			echo "error";
+			exit;
+		}
+
+
+		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
+
+
+		$id=($_GET['id']);
+		$name=($_GET['name']);
+		$info=($_GET['info']);
+		$token=($_GET['token']);
+
+        $sql = "INSERT into devices values ('null' , '" . $id . "' , '" . $name . "' , '" . $info . "' , '" . $token . "')";
+        $insert=$dbh->query($sql);
+         if($insert){
+            $sql= "Select * from devices where user_id= '" . $id . "' and token= '" . $token . "' ";
+            $devices=$dbh->query($sql);
+            $i=0;
+            foreach ($devices as $row) {
+                $i=$i + 1;
+                 echo $row['id'];
+            }
+             if($i==0){
+                         echo "error";
+                   exit;
+             }
+         }
+         else{
+            echo "error";
+         }
+
+         //$statement=$dbh->prepare("SELECT * FROM devices where user_id = ? ");
+         //$statement->execute(array($id));
+         //$results=$statement->fetchAll(PDO::FETCH_ASSOC);
+         //$json=json_encode($results);
+         //echo $json;
+
+?>
