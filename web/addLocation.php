898c20e3b6c004924d2dc43c9eb6928e8001b384
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-15 12:22:13 +0200

    Laugarren igoera, updateDevice

--- web/addLocation.php
+++ web/addLocation.php
@@ -8,6 +8,15 @@
 
 		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
 
+ $sql1 = "INSERT into locations values ('1' , '1' , '1' , '1' )";
+        $insert=$dbh->query($sql1);
+         if($insert){
+            echo "ok";
+         }
+         else{
+            echo "error2";
+            echo $insert;
+         }
 
 		$id=($_GET['id']);
 		$long=($_GET['long']);
