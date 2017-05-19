cab9616475573e8b29ee8534d12f5875ee957682
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-19 11:19:18 +0200

    Laugarren igoera, updateDevice

--- web/addLocation.php
+++ web/addLocation.php
@@ -1,17 +1,21 @@
 <?php
 
-		if( !(isset($_SERVER['id']))){
-			echo "error";
-			exit;
-		}
+		//if( !(isset($_SERVER['id']))){
+		//	echo "error";
+		//	exit;
+		//}
 
 
 		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
 
 
-		$id=($_SERVER['id']);
-		$long=($_POST['longitude']);
-		$lat=($_POST['latitude']);
+		//$id=($_SERVER['id']);
+		//$long=($_POST['longitude']);
+		//$lat=($_POST['latitude']);
+
+        $id=1;
+		$long=1;
+		$lat=1;
 
         $sql = "SELECT MAX(id) + 1 FROM locations";
         $statement = $dbh->prepare($sql);
