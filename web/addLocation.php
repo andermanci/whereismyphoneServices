47bf8f67d830b2134ba7066ea5c69ba6818268c2
UrtziOtamendi <urtziotamendi@gmail.com>
2017-05-19 11:43:20 +0200

    Laugarren igoera, updateDevice

--- web/addLocation.php
+++ web/addLocation.php
@@ -8,14 +8,15 @@
 
 		$dbh = new PDO("pgsql:dbname=d32n963hbr8ngb;host=ec2-79-125-125-97.eu-west-1.compute.amazonaws.com;port=5432", "ofwsfkgedzztju", "fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98");
 
-
+        $data = file_get_contents('php://input');
+        $json = json_decode($data, true);
 		//$id=($_SERVER['id']);
 		//$long=($_POST['longitude']);
 		//$lat=($_POST['latitude']);
 
         $id=1;
-		$long=1;
-		$lat=1;
+		$long=$json['longitude'];
+		$lat=$json['latitude'];
 
         $sql = "SELECT MAX(id) + 1 FROM locations";
         $statement = $dbh->prepare($sql);
